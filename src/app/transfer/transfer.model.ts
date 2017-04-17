59deff5d227847a1489e840f36e3b9105ad7123a
Roy Kumowal <14043337@BTPN>
2017-04-17 14:32:24 +0700

    initial release to hosting server (DO)

--- src/app/transfer/transfer.model.ts
+++
@@ -1,7 +0,0 @@
-import { Rekening } from '../rekening/rekening.model';
-
-export class Transfer {
-    constructor(public sumber: Rekening, public tujuan : Rekening, public nilai : number, public berita : string) {
-
-    }
-}
